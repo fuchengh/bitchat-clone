64b27b4ee61b812984bafb0292fe66c46b2ee369
Fred Hsieh <13806592+fuchengh@users.noreply.github.com>
2025-08-25 16:25:23 -0700

    feat: on_line handler for bitchatd

--- src/daemon/main.cpp
+++ src/daemon/main.cpp
@@ -3,8 +3,27 @@
 #include "util/constants.hpp"
 #include "util/log.hpp"
 
+static std::atomic<bool> g_tail_enabled{false};
+
 static void on_line(const std::string &line)
 {
+    if (line == "QUIT")
+    {
+        LOG_INFO("Received QUIT command, exiting...");
+        return;
+    }
+    if (line == "TAIL on")
+    {
+        g_tail_enabled.store(true);
+        LOG_INFO("TAIL Enabled");
+        return;
+    }
+    if (line == "TAIL off")
+    {
+        g_tail_enabled.store(false);
+        LOG_INFO("TAIL Disabled");
+        return;
+    }
     LOG_INFO("CMD: %s", line.c_str());
 }
 
